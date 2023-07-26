ff13ce46a0d266cb32d13d9635cceabb2196c2e5
bhama <bhama@Shree>
2023-07-26 23:53:18 +0530

    First commit

---
+++ Day 1/src/org/tnsif/operators/LogicalOperatorsDemo.java
@@ -0,0 +1,15 @@
+//Program to Demonstrate Logical Operators.
+package org.tnsif.operators;
+
+public class LogicalOperatorsDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println(31!=7 && 3>=7);	
+		System.out.println(31!=7 || 3<=7);
+		System.out.println(!(31!=7 || 3<=7));
+		
+	}
+		
+
+}
